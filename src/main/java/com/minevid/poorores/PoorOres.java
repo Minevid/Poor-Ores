a11f8c7120e3aed0a9be8ea1cc997e36963bd42d
Reynout De Wolf <reynout.dewolf.y0468@student.hogent.be>
2017-12-26 01:08:28 +0100

    Begin

---
+++ src/main/java/com/minevid/poorores/PoorOres.java
@@ -0,0 +1,28 @@
+package com.minevid.poorores;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="poorores", name = "Poor Ores", version = "1.0")
+public class PoorOres {
+
+    @Mod.Instance("poorores")
+    public static PoorOres instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
