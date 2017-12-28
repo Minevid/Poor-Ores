6dbe1f5dce97a41e50da8d776c83e5f85c84b2c3
Reynout De Wolf <reynout.dewolf.y0468@student.hogent.be>
2017-12-28 21:02:10 +0100

    Items begin

---
+++ src/main/java/com/minevid/poorores/init/ModItems.java
@@ -0,0 +1,11 @@
+package com.minevid.poorores.init;
+
+public class ModItems {
+
+
+    public static void init(){
+
+    }
+
+    // TODO: 28/12/2017 Think about Init Model inside EVERY ItemClass/Not
+}
